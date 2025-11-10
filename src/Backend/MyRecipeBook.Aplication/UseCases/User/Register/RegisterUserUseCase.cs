b1fd8c34b3f335da69abb5773174fb15a89e1cd3
Thalys M <thalyscorreia@hotmail.com>
2025-11-10 13:45:13 -0300

    feat: use case users

---
+++ src/Backend/MyRecipeBook.Aplication/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase : IRegisterUserUseCase
+    {
+
+        private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+        private readonly IUserReadOnlyRepository _readOnlyRepository;
+        private readonly IMapper _mapper;
+        private readonly PasswordEncripter _passwordEncrpter;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RegisterUserUseCase(IUserWriteOnlyRepository writeOnlyRepository, IUserReadOnlyRepository readOnlyRepository, IMapper mapper, PasswordEncripter passwordEncrpter, IUnitOfWork unitOfWork)
+        {
+            _writeOnlyRepository = writeOnlyRepository;
+            _readOnlyRepository = readOnlyRepository;
+            _passwordEncrpter = passwordEncrpter;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
+        {
+
+            await Validate(request);
+
+            var user = _mapper.Map<Domain.Entities.User>(request);
+
+            user.Password = _passwordEncrpter.Encrypt(request.Password);
+
+            await _writeOnlyRepository.Add(user);
+
+            await _unitOfWork.Commit();
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+            };
+        }
+
+        private async Task Validate(RequestRegisterUserJson request)
+        {
+           var validator = new RegisterUserValidator();
+
+           var result = validator.Validate(request);
+
+           var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+            if(emailExist)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessageException.EMAIL_ALREADY_REGISTERED));
+            }
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Aplication/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,23 @@
+﻿
+using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(user => user.Name)
+                .NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY)
+                .MaximumLength(100).WithMessage(ResourceMessageException.NAME_EXCEED);
+            RuleFor(user => user.Email)
+                .NotEmpty().WithMessage(ResourceMessageException.EMAIL_EMPTY)
+                .EmailAddress().WithMessage(ResourceMessageException.EMAIL_INVALID);
+            RuleFor(user => user.Password)
+                .NotEmpty().WithMessage(ResourceMessageException.PASSWORD_EMPTY)
+                .MinimumLength(6).WithMessage(ResourceMessageException.PASSWORD_INVALID);
+        }
+    }
+}
